3f7608c364f6ee73bb677b2a2bb65aae81dbe610
nku bende <bende@amalytics.co>
2018-11-13 23:12:25 +0000

    added new steps

--- features/view1/view1.feature
+++ features/view1/view1.feature
@@ -13,7 +13,15 @@ Feature: landing page
       | More from Which? |
 
     And the TV & home entertainment link is present
-
+    And the page elemnets tabs are displayed
+      | All Televisions    |
+      | Best Buys (59)     |
+      | Don't Buys (22)    |
+      | Advice Guides (20) |
+    And i click on "All Televisions" on the element tabs
+    And i click on "Best Buys (59)" on the element tabs
+    And i click on "Don't Buys (22)" on the element tabs
+    And i click on "Advice Guides (20)" on the element tabs
 
   Scenario: navigate to tv & home entertainment page
     Given I visit "/reviews/televisions"
@@ -28,4 +36,9 @@ Feature: landing page
       | More from Which? |
     When the user click on the tv&home entertainment link
     Then the user navigates to TV & home entertainment
+
+Scenario: navigate to reviews
+  Given I visit "/reviews"
+  Then the review page is displayed
+  And the "url" is displayed
 
--- features/view1/view1.rb
+++ features/view1/view1.rb
@@ -14,6 +14,18 @@ And(/^the TV & home entertainment link is present$/) do
   page.has_content?('TV & home entertainment')
 end
 
+And(/^the page elemnets tabs are displayed$/) do |components|
+  # table is a table.hashes.keys # => [:All Televisions]
+  components.hashes.each do |row|
+    puts 'found report components column header [' + row['column Name'].to_s + ']'
+    expect(page.has_content?(row['column Name'])).to eq(true)
+  end
+end
+
+And(/^i click on "([^"]*)" on the element tabs$/) do |element|
+  find('._2IGkQ ._2O9Lw li', :text => element).click()
+end
+
 When(/^the user click on the tv&home entertainment link$/) do
   find('.c_UoW').click()
 end
@@ -25,4 +37,17 @@ end
 And(/^the user navigate to Technology conponent$/) do
   pending
 end
+
+Then(/^the review page is displayed$/) do
+  page.has_content?('Our investigation exposes fake reviews')
+end
+
+And(/^the "([^"]*)" is displayed$/) do |url|
+  if url == 'We reveal our top-choice wireless speakers of the month'
+  end
+  puts 'We reveal our top-choice wireless speakers of the month'
+end
+
+
+
 
